7019ca57604c2870b6f8d836ee90678aac87cb5c
shaghayegh hosseini <shaghayegh.hoseini93@gmail.com>
2022-02-02 14:09:34 +0330

    add Announcement folder

---
+++ src/components/Announcement/Announcement.js
@@ -0,0 +1,10 @@
+import "./announcement.css";
+const Announcement = () => {
+  return (
+    <div className="announceContainer">
+      Super Deal! Free Shipping on Orders Over $50
+    </div>
+  );
+};
+
+export default Announcement;
