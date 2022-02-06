13efbeca3c38b8547c916ffd06f12949f074d2b9
shaghayegh hosseini <shaghayegh.hoseini93@gmail.com>
2022-02-06 15:47:52 +0330

    render slider and categories component

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,7 +1,14 @@
+import Categories from "../components/Categories/Categories";
+import Slider from "../components/Slider/Slider";
 import Layout from "../Layout/Layout";
 
 const HomePage = () => {
-  return <Layout><h2>HomePage</h2></Layout>;
+  return (
+    <Layout>
+      <Slider />
+      <Categories />
+    </Layout>
+  );
 };
 
 export default HomePage;
