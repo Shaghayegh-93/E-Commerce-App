2b695a30255f6991efd354fc5c20eb6ec093df78
shaghayegh hosseini <shaghayegh.hoseini93@gmail.com>
2022-02-06 14:09:38 +0330

    add data file for data that will be needed in this project

---
+++ src/data.js
@@ -0,0 +1,23 @@
+export const sliderItems = [
+  {
+    id: 1,
+    img: "https://i.ibb.co/DG69bQ4/2.png",
+    title: "SUMMER SALE",
+    desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+    bg: "#fcf1ed",
+  },
+  {
+    id: 2,
+    img: "https://i.ibb.co/cXFnLLV/3.png",
+    title: "AUTUMN COLLECTION",
+    desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+    bg: "#fbf0f4",
+  },
+  {
+    id: 3,
+    img: "https://preview.colorlib.com/theme/estore/assets/img/hero/xhero_man.png.pagespeed.ic.cN86RxAvqq.webp",
+    title: "LOUNGEWEAR LOVE",
+    desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+    bg: "#f0f9ff",
+  },
+];
