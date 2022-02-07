a2dc4573cc3fbb4882c601925895850fd4600118
shaghayegh hosseini <shaghayegh.hoseini93@gmail.com>
2022-02-07 14:00:43 +0330

    render ProductList

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,12 +1,14 @@
 import Categories from "../components/Categories/Categories";
 import Slider from "../components/Slider/Slider";
 import Layout from "../Layout/Layout";
+import ProductList from "../components/ProductList/ProductList";
 
 const HomePage = () => {
   return (
     <Layout>
       <Slider />
       <Categories />
+      <ProductList />
     </Layout>
   );
 };
