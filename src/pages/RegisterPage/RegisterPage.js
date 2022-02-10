cfc3e43d07e73f5207f0c6fd5ecf75d03011276e
shaghayegh hosseini <shaghayegh.hoseini93@gmail.com>
2022-02-10 18:20:59 +0330

    add Register page

---
+++ src/pages/RegisterPage/RegisterPage.js
@@ -0,0 +1,11 @@
+import Register from "../../components/Register/Register";
+
+const RegisterPage = () => {
+  return (
+    <div className="container">
+      <Register />
+    </div>
+  );
+};
+
+export default RegisterPage;
