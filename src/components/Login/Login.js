d8891af29b98725a281dbc57a822ba8035d980cc
shaghayegh hosseini <shaghayegh.hoseini93@gmail.com>
2022-02-11 16:48:59 +0330

    add classname to inputs

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -5,8 +5,8 @@ const Login = () => {
       <div className={styles.wrapper}>
         <h1 className={styles.title}>SIGN IN</h1>
         <form className={styles.formControl}>
-          <input placeholder="username" />
-          <input placeholder="password" />
+          <input className={styles.input} placeholder="username" />
+          <input className={styles.input} placeholder="password" />
           <button className={styles.button}>LOGIN</button>
           <a className={styles.link}>DO NOT YOU REMEMBER THE PASSWORD?</a>
           <a className={styles.link}>CREATE A NEW ACCOUNT</a>
--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -5,12 +5,12 @@ const Register = () => {
       <div className={styles.wrapper}>
         <h1 className={styles.title}>CREATE AN ACCOUNT</h1>
         <form className={styles.formControl}>
-          <input placeholder="name" />
-          <input placeholder="last name" />
-          <input placeholder="username" />
-          <input placeholder="email" />
-          <input placeholder="password" />
-          <input placeholder="confirm password" />
+          <input className={styles.input} placeholder="name" />
+          <input className={styles.input} placeholder="last name" />
+          <input className={styles.input} placeholder="username" />
+          <input className={styles.input} placeholder="email" />
+          <input className={styles.input} placeholder="password" />
+          <input className={styles.input} placeholder="confirm password" />
           <span className={styles.aggrement}>
             By creating an account, I consent to the processing of my personal
             data in accordance with the <b>PRIVACY POLICY</b>
