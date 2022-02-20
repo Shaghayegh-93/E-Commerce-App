61fd95da6870bf516e52167c57e48e5684faa61d
shaghayegh hosseini <shaghayegh.hoseini93@gmail.com>
2022-02-20 17:29:50 +0330

    add footer component to layout of project

--- src/Layout/Layout.js
+++ src/Layout/Layout.js
@@ -1,12 +1,15 @@
 import Announcement from "../components/Announcement/Announcement";
+import Footer from "../components/Footer/Footer";
 import Navigation from "../components/Navigation/navigation";
+import NewsLetters from "../components/NewsLetters/NewsLetters";
 
 const Layout = ({ children }) => {
   return (
-    <div>
-      <Announcement/>
+    <div style={{ overflowX: "hidden" }}>
       <Navigation />
+      <Announcement />
       {children}
+      <Footer />
     </div>
   );
 };
