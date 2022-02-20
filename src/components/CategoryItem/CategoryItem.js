7a9b46d38f3fb031c161558b3519156906b53ae4
shaghayegh hosseini <shaghayegh.hoseini93@gmail.com>
2022-02-20 17:28:32 +0330

    add Link component for redirect client to the category that they want

--- src/components/CategoryItem/CategoryItem.js
+++ src/components/CategoryItem/CategoryItem.js
@@ -1,12 +1,15 @@
 import styles from "./categoryItem.module.css";
+import { Link } from "react-router-dom";
 const CategoryItem = ({ categoryItem }) => {
   return (
     <div className={styles.categoryContainer}>
-      <img className={styles.categoryImg} src={categoryItem.img}></img>
+      <Link to={`/Products/${categoryItem.cat}`}>
+      <img alt="clothesImg" className={styles.categoryImg} src={categoryItem.img}></img>
       <div className={styles.categoryInfoContainer}>
         <h1 className={styles.categoryTitle}>{categoryItem.title}</h1>
         <button className={styles.categoryButton}>SHOP NOW</button>
       </div>
+      </Link>
     </div>
   );
 };
