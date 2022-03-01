69e19ec3ce4cb19578862116828db81de6dc68f8
shaghayegh hosseini <shaghayegh.hoseini93@gmail.com>
2022-03-01 13:34:07 +0330

    imprt cartProvider component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import routes from "./routes";
+import CartProvider from "./context/CartProvider";
 
 function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        {routes.map((route) => (
-          <Route key={route.id} {...route} />
-        ))}
-      </Routes>
+      <CartProvider>
+        <Routes>
+          {routes.map((route) => (
+            <Route key={route.id} {...route} />
+          ))}
+        </Routes>
+      </CartProvider>
     </BrowserRouter>
   );
 }
