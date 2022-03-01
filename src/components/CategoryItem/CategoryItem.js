700a0a40efeacdc30f0a48d137fd2d51030450fe
shaghayegh hosseini <shaghayegh.hoseini93@gmail.com>
2022-03-01 13:36:38 +0330

    change the title(proprty of categoryItem in data)to name

--- src/components/CategoryItem/CategoryItem.js
+++ src/components/CategoryItem/CategoryItem.js
@@ -4,11 +4,15 @@ const CategoryItem = ({ categoryItem }) => {
   return (
     <div className={styles.categoryContainer}>
       <Link to={`/Products/${categoryItem.cat}`}>
-      <img alt="clothesImg" className={styles.categoryImg} src={categoryItem.img}></img>
-      <div className={styles.categoryInfoContainer}>
-        <h1 className={styles.categoryTitle}>{categoryItem.title}</h1>
-        <button className={styles.categoryButton}>SHOP NOW</button>
-      </div>
+        <img
+          alt="clothesImg"
+          className={styles.categoryImg}
+          src={categoryItem.img}
+        ></img>
+        <div className={styles.categoryInfoContainer}>
+          <h1 className={styles.categoryTitle}>{categoryItem.name}</h1>
+          <button className={styles.categoryButton}>SHOP NOW</button>
+        </div>
       </Link>
     </div>
   );
