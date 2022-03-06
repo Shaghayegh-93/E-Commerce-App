3a75b14ff6602f756c3e54691cd4bfdc9a5e0270
shaghayegh hosseini <shaghayegh.hoseini93@gmail.com>
2022-03-06 10:56:07 +0330

    use stripeCheckout component and usecontext

--- src/pages/CartPage/CartPage.js
+++ src/pages/CartPage/CartPage.js
@@ -1,13 +1,16 @@
 import { Add, Remove } from "@material-ui/icons";
+import StripeCheckout from "react-stripe-checkout";
+import { useCart } from "../../context/CartProvider";
 import Layout from "../../Layout/Layout";
 import styles from "./CartPage.module.css";
 
 const CartPage = () => {
+  const { cart, total } = useCart();
   return (
     <Layout>
       <div className={styles.mainContainer}>
         <h1 className={styles.title}>YOUR BAG</h1>
-        <div className={styles.TopContainer}>
+        <div className={styles.topContainer}>
           <button className={`${styles.topButton} ${styles.topButtonOutlined}`}>
             CONTINUE SHOPPING
           </button>
@@ -21,41 +24,46 @@ const CartPage = () => {
         </div>
         <div className={styles.bottomContainer}>
           <div className={styles.infoContainer}>
-            <div className={styles.product}>
-              <div className={styles.productDetail}>
-                <img
-                  className={styles.productDetailImg}
-                  src="https://hips.hearstapps.com/vader-prod.s3.amazonaws.com/1614188818-TD1MTHU_SHOE_ANGLE_GLOBAL_MENS_TREE_DASHERS_THUNDER_b01b1013-cd8d-48e7-bed9-52db26515dc4.png?crop=1xw:1.00xh;center,top&resize=480%3A%2A"
-                />
-                <div className={styles.details}>
-                  <span className={styles.productName}>
-                    <b>Product:</b> JESSIE THUNDER SHOES
-                  </span>
-                  <span className={styles.productId}>
-                    <b>ID:</b> 93813718293
-                  </span>
-                  <div className={styles.productColor}></div>
-                  <div className={styles.productSize}>
-                    <b>Size:</b> 37.5
+            {cart.map((product) => (
+              <div className={styles.product}>
+                <div className={styles.productDetail}>
+                  <img className={styles.productDetailImg} src={product.img} />
+                  <div className={styles.details}>
+                    <span className={styles.productName}>
+                      <b>Product:</b> {product.name}
+                    </span>
+                    <span className={styles.productId}>
+                      <b>ID:</b>
+                      {product._id}
+                    </span>
+                    <div className={styles.productColor}>{product.color}</div>
+                    <div className={styles.productSize}>
+                      <b>Size:</b>
+                      {product.size}
+                    </div>
                   </div>
                 </div>
-              </div>
-              <div className={styles.priceDetail}>
-                <div className={styles.productAmountContainer}>
-                  <Remove />
-                  <span className={styles.productAmount}>1</span>
-                  <Add />
+                <div className={styles.priceDetail}>
+                  <div className={styles.productAmountContainer}>
+                    <Remove />
+                    <span className={styles.productAmount}>
+                      {product.quantity}
+                    </span>
+                    <Add />
+                  </div>
+                  <span className={styles.productPrice}>
+                    ${product.price * product.quantity}
+                  </span>
                 </div>
-                <span className={styles.productPrice}>$30</span>
               </div>
-            </div>
+            ))}
             <hr className={styles.hr} />
           </div>
           <div className={styles.summaryContainer}>
             <h1 className={styles.summaryTitle}>ORDER SUMMARY</h1>
             <div className={styles.summaryItem}>
               <span className={styles.summaryItemText}>Subtotal</span>
-              <span className={styles.summaryItemPrice}>$80</span>
+              <span className={styles.summaryItemPrice}>${total}</span>
             </div>
             <div className={styles.summaryItem}>
               <span className={styles.summaryItemText}>Estimated Shipping</span>
@@ -67,9 +75,20 @@ const CartPage = () => {
             </div>
             <div className={`${styles.summaryItem} ${styles.summaryItemTotal}`}>
               <span className={styles.summaryItemText}>Total</span>
-              <span className={styles.summaryItemPrice}>$80</span>
+              <span className={styles.summaryItemPrice}>${total}</span>
             </div>
-            <button className={styles.summaryButton}>CHECKOUT NOW</button>
+            <StripeCheckout
+              name="Lama Shop"
+              image="https://avatars.githubusercontent.com/u/1486366?v=4"
+              billingAddress
+              shippingAddress
+              description={`Your total is $${total}`}
+              amount={total * 100}
+              // token={onToken}
+              // stripeKey={KEY}
+            >
+              <button className={styles.summaryButton}>CHECKOUT NOW</button>
+            </StripeCheckout>
           </div>
         </div>
       </div>
