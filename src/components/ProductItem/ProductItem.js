d7712389c0ffe4785bff424f8fdcc0cbaec01531
shaghayegh hosseini <shaghayegh.hoseini93@gmail.com>
2022-03-06 10:58:08 +0330

    add Link component to searchIcon

--- src/components/ProductItem/ProductItem.js
+++ src/components/ProductItem/ProductItem.js
@@ -2,6 +2,7 @@ import styles from "./productItem.module.css";
 import ShoppingCartOutlinedIcon from "@material-ui/icons/ShoppingCartOutlined";
 import SearchOutlinedIcon from "@material-ui/icons/SearchOutlined";
 import FavoriteBorderOutlinedIcon from "@material-ui/icons/FavoriteBorderOutlined";
+import { Link } from "react-router-dom";
 const ProductItem = ({ productItem }) => {
   return (
     <div className={styles.productItemContainer}>
@@ -12,7 +13,9 @@ const ProductItem = ({ productItem }) => {
           <ShoppingCartOutlinedIcon />
         </div>
         <div className={styles.productItemIcon}>
-          <SearchOutlinedIcon />
+          <Link to={`/product/${productItem._id}`}>
+            <SearchOutlinedIcon />
+          </Link>
         </div>
         <div className={styles.productItemIcon}>
           <FavoriteBorderOutlinedIcon />
