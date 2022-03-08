9a7ddc0617e21844e5f08fe92ca24a3ff2a9d24a
shaghayegh hosseini <shaghayegh.hoseini93@gmail.com>
2022-03-08 16:22:00 +0330

    import react toastify

--- src/App.js
+++ src/App.js
@@ -2,11 +2,14 @@ import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import routes from "./routes";
 import CartProvider from "./context/CartProvider";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function App() {
   return (
     <BrowserRouter>
       <CartProvider>
+        <ToastContainer theme="colored" />
         <Routes>
           {routes.map((route) => (
             <Route key={route.id} {...route} />
