aa2ee2b266d4fa1d12cecd599990501e1a4aa6b6
shaghayegh hosseini <shaghayegh.hoseini93@gmail.com>
2022-03-16 14:10:29 +0330

    clean the code

--- src/pages/LoginPage/LoginPage.js
+++ src/pages/LoginPage/LoginPage.js
@@ -1,9 +1,11 @@
 import Login from "../../components/Login/Login";
 
 const LoginPage = () => {
-    return ( <div>
-        <Login/>
-    </div> );
-}
- 
+  return (
+    <div>
+      <Login />
+    </div>
+  );
+};
+
 export default LoginPage;
