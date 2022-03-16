f852414a473fde6ff13b8b343cdb6b7816ccc3db
shaghayegh hosseini <shaghayegh.hoseini93@gmail.com>
2022-03-16 14:14:03 +0330

    import Layout component

--- src/pages/CheckoutPage/CheckoutPage.js
+++ src/pages/CheckoutPage/CheckoutPage.js
@@ -1,17 +1,20 @@
 import styles from "./checkoutPage.module.css";
 import { useCart } from "../../context/CartProvider";
 import { Link } from "react-router-dom";
+import Layout from "../../Layout/Layout";
 const CheckoutPage = () => {
   const { cart } = useCart();
   return (
-    <div className={styles.checkoutContainer}>
-      {cart
-        ? `Order has been created successfully. Your order number is 666544546`
-        : `Successfull. Your order is being prepared...`}
-      <Link to="/">
-        <button style={{ padding: 10, marginTop: 20 }}>Go to Homepage</button>
-      </Link>
-    </div>
+    <Layout>
+      <div className={styles.checkoutContainer}>
+        {cart
+          ? `Order has been created successfully. Your order number is 666544546`
+          : `Successfull. Your order is being prepared...`}
+        <Link to="/">
+          <button style={{ padding: 10, marginTop: 20 }}>Go to Homepage</button>
+        </Link>
+      </div>
+    </Layout>
   );
 };
 
