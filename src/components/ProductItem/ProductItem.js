acbf8d6d4dbbb89ec8d8c6adb28d20db05f4efdb
shaghayegh hosseini <shaghayegh.hoseini93@gmail.com>
2022-03-16 13:55:58 +0330

    add toggle state for toggleing the color of favorit icon

--- src/components/ProductItem/ProductItem.js
+++ src/components/ProductItem/ProductItem.js
@@ -3,21 +3,35 @@ import ShoppingCartOutlinedIcon from "@material-ui/icons/ShoppingCartOutlined";
 import SearchOutlinedIcon from "@material-ui/icons/SearchOutlined";
 import FavoriteBorderOutlinedIcon from "@material-ui/icons/FavoriteBorderOutlined";
 import { Link } from "react-router-dom";
+import { useState } from "react";
+
 const ProductItem = ({ productItem }) => {
+  const [toggle, setToggle] = useState(false);
+
+  const triggerToggle = () => {
+    setToggle(!toggle);
+  };
+  const favoritClass = toggle ? styles.favorit : "";
+
   return (
     <div className={styles.productItemContainer}>
       <div className={styles.circle}></div>
       <img className={styles.produtImg} src={productItem.img} />
       <div className={styles.productItemInfo}>
-        <div className={styles.productItemIcon}>
-          <ShoppingCartOutlinedIcon />
-        </div>
+        <Link to="/cart">
+          <div className={styles.productItemIcon}>
+            <ShoppingCartOutlinedIcon />
+          </div>
+        </Link>
         <div className={styles.productItemIcon}>
           <Link to={`/product/${productItem._id}`}>
             <SearchOutlinedIcon />
           </Link>
         </div>
-        <div className={styles.productItemIcon}>
+        <div
+          onClick={triggerToggle}
+          className={`${styles.productItemIcon} ${favoritClass}`}
+        >
           <FavoriteBorderOutlinedIcon />
         </div>
       </div>
